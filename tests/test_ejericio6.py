dfcf0f6997756206598e2dc597e15d26a0583d6b
santichac <santichac3@gmail.com>
2022-06-21 18:08:51 -0300

    Agregue test_ejercicio6.py

--- tests/test_ejericio6.py
+++ tests/test_ejericio6.py
@@ -47,8 +47,30 @@ def test_decodificacion_letras():
     Este test es para comprobar que la función decodificación
     funcione con palabras.
     """
-    palabra_o_numero = 'milanesa'
+    palabra_o_numero = 'okncpguc'
     posicionamiento = 2
-    resultado = codificacion(palabra_o_numero, posicionamiento)
+    resultado = decodificacion(palabra_o_numero, posicionamiento)
     assert isinstance(resultado, str), 'El resultado debe ser una cadena de letras.'
-    assert resultado == 'okncpguc', 'El resultado no es el esperado.'
+    assert resultado == 'milanesa', 'El resultado no es el esperado.'
+
+def test_decodificacion_numeros():
+    """
+    Este test es para comprobar que la función decodificación
+    funcione con números.
+    """
+    palabra_o_numero = '27'
+    posicionamiento = 1
+    resultado = decodificacion(palabra_o_numero, posicionamiento)
+    assert isinstance(resultado, str), 'El resultado debe ser una cadena de números.'
+    assert resultado == '16', 'El resultado no es el esperado.'
+
+def test_decodificacion_variado():
+    """
+    Este test es para comprobar que la función decodificación
+    funcione con palabras y números a la vez.
+    """
+    palabra_o_numero = '47 vrq orv sdmdurv'
+    posicionamiento = 3
+    resultado = decodificacion(palabra_o_numero, posicionamiento)
+    assert isinstance(resultado, str), 'El resultado debe ser una cadena de números y despues letras.'
+    assert resultado == '14 son los pajaros', 'El resultado no es el esperado.'
